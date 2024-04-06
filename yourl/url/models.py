1a659b1b649a3db71e6d3e8f347fde3ea4d42129
Bala Murali <baluulived966@gmail.com>
2024-04-06 17:06:31 +0530

    created model named UrlData to store ogurl(maxlen10000) and slug(maxlen11).
    Also mapped urls.py and created templates with main.html in it and mapped it to views.py.
    
    created a form to get the ogurl with label ="URL"

--- yourl/url/models.py
+++ yourl/url/models.py
@@ -1,3 +1,6 @@
 from django.db import models
-
-# Create your models here.
+class UrlData(models.Model):
+    url = models.CharField(max_length=10000)
+    slug = models.CharField(max_length=11)
+def __str__(self):
+        return f"Short Url for: {self.url} is {self.slug}"
--- yourl/yourl/settings.py
+++ yourl/yourl/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'url'
+    
+    
 ]
 
 MIDDLEWARE = [
@@ -47,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'yourl.urls'
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'yourl.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
